7639fb3d7961622f83673be5d3489349a754a39a
lar0129 <lianganran0129@163.com>
2025-10-25 16:32:39 +0000

    Deploying to gh-pages from @ lar0129/scholar@02748b28650b1b473e64a61d174215b127149613 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lar0129/al-folio-scholar-blog",
+    "data-repo": "lar0129/scholar",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
